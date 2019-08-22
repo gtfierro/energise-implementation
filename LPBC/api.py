d5cab3ea0fc479cbe0e6ba742ec096900234fe4b
ENERGISE <jasperpakshong@gmail.com>
2019-08-22 10:09:38 -0700

    api update

--- LPBC/api.py
+++ LPBC/api.py
@@ -2,7 +2,7 @@
 list of request commands:
     
 inverter:
-    - f"http://131.243.41.47:9090/control?inv_id={inv},Batt_ctrl={batt},pf_ctrl={pf_ctrl}"
+    - f"http://131.243.41.47:9090/control?Batt_ctrl={batt},pf_ctrl={pf_ctrl},inv_id={inv}"
 
 load racks:
     http://131.243.41.118:9090/control_enable
@@ -16,7 +16,28 @@ load racks:
         
 '''
 
+# load rack values:
+P_ctrl = 0
+group_id = 0 # [0,1,2]
+
+# inverter values:
+Batt_ctrl = 0
+pf_ctrl = 1
+inv_id = 2 # [1,2,3]
 
 import requests
-r = requests.get('http://131.243.41.118:9090/control?P_ctrl=0')
+
+# load racks:
+#r = requests.get('http://131.243.41.118:9090/control_enable’)
+#r = requests.get('http://131.243.41.118:9090/control_disable’)
+
+#r = requests.get(f'http://131.243.41.118:9090/control?P_ctrl={P_ctrl}’)
+#r = requests.get(f'http://131.243.41.118:9090/control?P_ctrl={P_ctrl},group_id={group_id}’)
+
+# inverter
+#r = requests.get(f'http://131.243.41.47:9090/control?Batt_ctrl={Batt_ctrl}')  # works
+#r = requests.get(f'http://131.243.41.47:9090/control?Batt_ctrl={Batt_ctrl},inv_id={inv_id}')  # works
+#r = requests.get(f'http://131.243.41.47:9090/control?Batt_ctrl={Batt_ctrl},pf_ctrl={pf_ctrl}')  # ?
+#r = requests.get(f'http://131.243.41.47:9090/control?Batt_ctrl={Batt_ctrl},pf_ctrl={pf_ctrl},inv_id={inv_id}')  # ?
+
 print(r)
