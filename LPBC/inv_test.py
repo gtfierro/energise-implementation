ce9ceb033f6ae7497dec2e454c3345a9ea9a5e4f
ENERGISE <jasperpakshong@gmail.com>
2019-08-22 16:07:33 -0700

    inv test

--- LPBC/inv_test.py
+++ LPBC/inv_test.py
@@ -930,7 +930,7 @@ for key in lpbcidx:
     cfg['entity'] = entitydict[lpbcCounter] #entity is like a key for each LPBC
     if actType == 'inverter':
         cfg['rate'] = 5 # JASPER CHANGE RATE HERE
-        cfg['local_channels'] = np.concatenate([pmu123Channels[pmu123_plugs_dict[key]], pmu123Channels[3 + pmu123_plugs_dict[key]], pmu123Channels[pmu123_plugs_dict[key]]]).tolist()
+        cfg['local_channels'] = np.concatenate([pmu123Channels[pmu123_plugs_dict[key]], pmu123Channels[3 + pmu123_plugs_dict[key]]]).tolist()
         #cfg['local_channels'] = np.concatenate([pmu123PChannels[pmu123P_plugs_dict[key]], pmu123Channels[3 + pmu123_plugs_dict[key]], pmu123Channels[pmu123_plugs_dict[key]]])
         #takes voltage measurements from PMU123P, current from PMU123, voltage measurements from PMU123P
         cfg['reference_channels'] = refChannels[pmu0_plugs_dict[key]].tolist() #listen to the correct references
