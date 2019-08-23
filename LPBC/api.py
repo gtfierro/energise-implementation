38d47e341e71eddcdc0507767bbb03e92c791463
ENERGISE <jasperpakshong@gmail.com>
2019-08-23 11:37:43 -0700

    flexlab commands

--- LPBC/api.py
+++ LPBC/api.py
@@ -26,11 +26,11 @@ P_ctrl = 0
 group_id = 0 # [0,1,2]
 
 # inverter values:
-Batt_ctrl = 0 # (+) is charging!
-pf_ctrl = 0.9 # [-1,1] - BUT abs() > 0.85
-inv_id = 3 # [1,2,3]
+Batt_ctrl = 100 # (+) is charging!
+pf_ctrl = 0.99 # [-1,1] - BUT abs() > 0.85
+inv_id = 1 # [1,2,3]
 
-inv_perc = 97
+inv_perc = 2
 
 
 if np.abs(pf_ctrl) < 0.85:
@@ -41,29 +41,31 @@ t0 = time.time()
 #~~~~~~~~~~~
 # LOAD RACKS:
 
-#r = requests.get('http://131.243.41.118:9090/control_enable')
-#r = requests.get('http://131.243.41.118:9090/control_disable')
+#command = 'http://131.243.41.118:9090/control_enable'
+#command = 'http://131.243.41.118:9090/control_disable'
 
-#r = requests.get(f'http://131.243.41.118:9090/control?P_ctrl={P_ctrl}')
-#r = requests.get(f'http://131.243.41.118:9090/control?P_ctrl={P_ctrl},group_id={group_id}')
+#command = f'http://131.243.41.118:9090/control?P_ctrl={P_ctrl}'
+#command = f'http://131.243.41.118:9090/control?P_ctrl={P_ctrl},group_id={group_id}'
 
 #~~~~~~~~~~~
 # INVERTER
 
 ##### batt only
-r = requests.get(f'http://131.243.41.47:9090/control?Batt_ctrl={Batt_ctrl}')  # works
+#command = f'http://131.243.41.47:9090/control?Batt_ctrl={Batt_ctrl}'  # works
 #####  pf only
-#r = requests.get(f'http://131.243.41.47:9090/control?pf_ctrl={pf_ctrl}') # ?
+#command = f'http://131.243.41.47:9090/control?pf_ctrl={pf_ctrl}' # works
 #####  batt / inv
-#r = requests.get(f'http://131.243.41.47:9090/control?Batt_ctrl={Batt_ctrl},inv_id={inv_id}')  # works
+#command = f'http://131.243.41.47:9090/control?Batt_ctrl={Batt_ctrl},inv_id={inv_id}'  # works
 ##### batt / pf
-#r = requests.get(f'http://131.243.41.47:9090/control?Batt_ctrl={Batt_ctrl},pf_ctrl={pf_ctrl}')  # no pf cmd
+#command = f'http://131.243.41.47:9090/control?Batt_ctrl={Batt_ctrl},pf_ctrl={pf_ctrl}'  # no pf cmd? had no enable command
 ##### batt / pf / inv
-#r = requests.get(f'http://131.243.41.47:9090/control?Batt_ctrl={Batt_ctrl},pf_ctrl={pf_ctrl},inv_id={inv_id}')  # works
+#command = f'http://131.243.41.47:9090/control?Batt_ctrl={Batt_ctrl},pf_ctrl={pf_ctrl},inv_id={inv_id}'  # works
 
 ##### inv perc
-#r = requests.get(f'http://131.243.41.47:9090/control?P_ctrl={inv_perc}')
+#command = f'http://131.243.41.47:9090/control?P_ctrl={inv_perc}'
+
+r = requests.get(command)
 
 print(f'time to execute: {time.time()-t0}')
 print(r)
-print('api cmd', dt.datetime.now())
+print('api cmd:', command, dt.datetime.now())
