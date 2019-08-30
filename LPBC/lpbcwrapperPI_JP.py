3a317c52da4869605fd67b8855ddb262767a1a92
ENERGISE <jasperpakshong@gmail.com>
2019-08-29 17:05:18 -0700

    jasper

--- LPBC/lpbcwrapperPI_JP.py
+++ LPBC/lpbcwrapperPI_JP.py
@@ -62,8 +62,8 @@ class lpbcwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
             # controller gains must be list, even if single phase. can use different gains for each phase
             # e.g. if only actuating on 2 phases (B and C) just put gains in order in list: [#gain B, #gain C]
             print('made a PI controller')
-            kp_ang = [0.0034]
-            ki_ang = [0.0677]
+            kp_ang = [0.0034*2.5]
+            ki_ang = [0.0677*1.5]
             kp_mag = [0.5670]
             ki_mag = [3.4497]
             self.controller = PIcontroller(nphases, kp_ang, ki_ang, kp_mag, ki_mag)
