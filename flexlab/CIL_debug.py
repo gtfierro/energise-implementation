1a9a436c54ab42b9435b201c929db4ebb29af92a
leo32393 <leo32393@berkeley.edu>
2020-03-19 13:45:44 -0700

    debug

--- flexlab/CIL_debug.py
+++ flexlab/CIL_debug.py
@@ -21,7 +21,7 @@ client = ModbusClient(IP, port=PORT)
 
 try:
     client.connect()
-    client.write_registers(50000, 1, unit=id)
+    client.write_registers(1, int(50000), unit=id)
     print('sent')
 
 except Exception as e:
