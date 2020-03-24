f92e2a0630413400421cd7b74ad82725ea67b27e
leo32393 <leo32393@berkeley.edu>
2020-03-24 12:26:53 -0700

    debug

--- LPBC/lpbcwrapperPI_JP.py
+++ LPBC/lpbcwrapperPI_JP.py
@@ -710,18 +710,15 @@ class lpbcwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
         print(reference_phasors[0][0])
         print(reference_phasors[1][0])
         print(reference_phasors[2][0])
-        print('upmu123p voltage: ')
+        print('upmu4 voltage: ')
         print('PHASE A: ',local_phasors[0][0])
         print('PHASE B: ',local_phasors[1][0])
         print('PHASE C: ', local_phasors[2][0])
-        print('123pcurrent: ')
+        print('upmu4 current: ')
         print('PHASE A: ',local_phasors[3][0])
         print('PHASE B: ',local_phasors[4][0])
         print('PHASE C: ', local_phasors[5][0])
-        print('0current')
-        print('PHASE A: ',local_phasors[3][0])
-        print('PHASE B: ',local_phasors[4][0])
-        print('PHASE C: ', local_phasors[5][0])
+
 
 
         iterstart = pytime.time()
