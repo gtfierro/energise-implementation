319f850a37c23e24b8397d1e446c4001221765c8
leo32393 <leo32393@berkeley.edu>
2020-04-06 13:13:25 -0700

    gains

--- LPBC/lpbcwrapperPI_CIL_HIL2.py
+++ LPBC/lpbcwrapperPI_CIL_HIL2.py
@@ -85,8 +85,8 @@ class lpbcwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
             
             #3.3
 # =============================================================================
-            alph = 0.55
-            beta = 0.55
+            alph = 0.6
+            beta = 0.3
             kp_ang = [0.0034*alph,0.0034*alph,0.0034*alph]
             ki_ang = [0.0677*alph,0.0677*alph,0.0677*alph]
             kp_mag = [0.1750*beta,0.3063*beta,0.8331*beta]
