a1504bd8f9e60ab32ff7450be39c1d10acc9ef76
leo32393 <leo32393@berkeley.edu>
2020-04-23 19:44:55 -0700

    ICDI

--- SPBC/spbc-example-jasper.py
+++ SPBC/spbc-example-jasper.py
@@ -328,35 +328,35 @@ class myspbc(pbc.SPBCProcess):
                     '''CHANGED INDICIES FOR T12 CONFIG ONLY'''
                     # Vtargdict[key]['Vmag'] = [cons_Vmag[0]-refphasor[0,0],cons_Vmag[1]-refphasor[1,0],cons_Vmag[2]-refphasor[2,0]]
                     # Vtargdict[key]['Vang'] = [cons_Vang[0]-refphasor[0,1],cons_Vang[1]-refphasor[1,1],cons_Vang[2]-refphasor[2,1]]
-                    if self.iteration < 13:
+                    if self.iteration < 13*2:
                         Vtargdict[key]['Vmag'] = [cons_Vmag[0]-refphasor[0,0],cons_Vmag[1]-refphasor[0,0],cons_Vmag[2]-refphasor[0,0]]
                         Vtargdict[key]['Vang'] = [cons_Vang[0]-refphasor[0,1],cons_Vang[1]-refphasor[0,1],cons_Vang[2]-refphasor[0,1]]
                     # if self.iteration > 31:
                     #     Vtargdict[key]['Vmag'] = [0.98 - refphasor[0, 0], 0.98 - refphasor[1, 0],0.98 - refphasor[2, 0]]
                     #     Vtargdict[key]['Vang'] = [-2 - refphasor[0, 1], -122 - refphasor[1, 1], 118 - refphasor[2, 1]]
-                    if 13 <= self.iteration < 26: #Change here if we want to set varying targets
-                        if '671_a' and '652_a' and '692_a' not in Qsat_nodes:
+                    if 13*2 <= self.iteration < 26*2: #Change here if we want to set varying targets
+                        if '671_a' and '671_b' and '671_c' not in Qsat_nodes:
                             Vtargdict[key]['Vmag'] = [0.96 - refphasor[0, 0], 0.96 - refphasor[0, 0],0.96 - refphasor[0, 0]]
-                        if '671_a' and '652_a' and '692_a' not in Psat_nodes:
+                        if '671_a' and '671_b' and '671_c' not in Psat_nodes:
                             Vtargdict[key]['Vang'] = [-2 - refphasor[0, 1], -2 - refphasor[0, 1], -2 - refphasor[0, 1]]
-                        if '671_a' and '652_a' and '692_a' in Psat_nodes:
+                        if '671_a' and '671_b' and '671_c' in Psat_nodes:
                             self.P_flag = []
                             self.P_flag.append(1)
-                        if '671_a' and '652_a' and '692_a' in Qsat_nodes:
+                        if '671_a' and '671_b' and '671_c' in Qsat_nodes:
                             self.Q_flag = []
                             self.Q_flag.append(1)
 
-                    elif self.iteration >= 26: #Change here if we want to set varying targets
-                        if '671_a' and '652_a' and '692_a' not in Psat_nodes:
+                    elif self.iteration >= 26*2: #Change here if we want to set varying targets
+                        if '671_a' and '671_b' and '671_c' not in Psat_nodes:
                             Vtargdict[key]['Vang'] = [-3 - refphasor[0, 1], -3 - refphasor[0, 1], -3 - refphasor[0, 1]]
 
-                        if '671_a' and '652_a' and '692_a' not in Qsat_nodes:
+                        if '671_a' and '671_b' and '671_c' not in Qsat_nodes:
                             Vtargdict[key]['Vmag'] = [0.94 - refphasor[0, 0], 0.94 - refphasor[0, 0],
                                                       0.94 - refphasor[0, 0]]
-                        if '671_a' and '652_a' and '692_a' in Psat_nodes:
+                        if '671_a' and '671_b' and '671_c' in Psat_nodes:
                             self.P_flag = []
                             self.P_flag.append(2)
-                        if '671_a' and '652_a' and '692_a' in Qsat_nodes:
+                        if '671_a' and '671_b' and '671_c' in Qsat_nodes:
                             self.Q_flag = []
                             self.Q_flag.append(2)
 
