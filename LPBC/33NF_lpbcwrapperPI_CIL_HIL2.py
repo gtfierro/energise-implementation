a1c05206ea577ebebdd013ca1b313af4162686fc
leo32393 <leo32393@berkeley.edu>
2020-05-07 16:39:37 -0700

    gains

--- LPBC/33NF_lpbcwrapperPI_CIL_HIL2.py
+++ LPBC/33NF_lpbcwrapperPI_CIL_HIL2.py
@@ -106,10 +106,10 @@ class lpbcwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
             # =============================================================================
             alph = 0.2
             beta = 1
-            kp_ang = [0.01*alph]
+            kp_ang = [0.005*alph]
             ki_ang = [0.3*alph]
-            kp_mag = [0.05*beta]
-            ki_mag = [1*beta]
+            kp_mag = [0.03*beta]
+            ki_mag = [1.2*beta]
 
             self.controller = PIcontroller(nphases, kp_ang, ki_ang, kp_mag, ki_mag)
         elif self.controllerType == 'LQR':
