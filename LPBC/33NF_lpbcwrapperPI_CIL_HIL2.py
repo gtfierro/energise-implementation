584fb16d823853f9518bc5239355072dd9029b7c
leo32393 <leo32393@berkeley.edu>
2020-05-08 11:44:36 -0700

    retuning

--- LPBC/33NF_lpbcwrapperPI_CIL_HIL2.py
+++ LPBC/33NF_lpbcwrapperPI_CIL_HIL2.py
@@ -107,9 +107,9 @@ class lpbcwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
 
             alph = 0.2
             beta = 0.7
-            kp_ang = [0.004* alph,0.001 * alph,0.01 * alph]
-            ki_ang = [0.3 * alph,0.1 * alph,0.3 * alph]
-            kp_mag = [0.01 * beta,0.005 * beta,0.05 * beta]
+            kp_ang = [0.004* alph,0.0007 * alph,0.01 * alph]
+            ki_ang = [0.3 * alph,0.04 * alph,0.1 * alph]
+            kp_mag = [0.01 * beta,0.01 * beta,0.05 * beta]
             ki_mag = [0.8 * beta,0.3 * beta,1 * beta]
 
             self.controller = PIcontroller(nphases, kp_ang, ki_ang, kp_mag, ki_mag)
