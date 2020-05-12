a70bd6a2e25f8003907e2e36fca158f94bb0da7c
leo32393 <leo32393@berkeley.edu>
2020-05-12 16:09:41 -0700

    debug

--- LPBC/lpbcwrapperPI_LR_CIL_HIL2_33NF.py
+++ LPBC/lpbcwrapperPI_LR_CIL_HIL2_33NF.py
@@ -1011,7 +1011,7 @@ elif testcase == '13bal':
 elif testcase == 'manual':
     lpbcidx = ['26'] #nodes of actuation
     key = '26'
-    acts_to_phase_dict[key] = np.asarray(['A','B','C']) #which phases to actuate for each lpbcidx # INPUT PHASES
+    acts_to_phase_dict[key] = np.asarray(['A','','']) #which phases to actuate for each lpbcidx # INPUT PHASES
     actType_dict[key] = 'inverter' #choose: 'inverter', 'load', or 'modbus'
 
 #these should be established once for the FLexlab,
