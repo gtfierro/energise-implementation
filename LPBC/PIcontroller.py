cd2d27dc2f8cf41b87c180482ff1ead480ea8d65
leo32393 <leo32393@berkeley.edu>
2020-05-13 11:42:53 -0700

    Qdel/Pmag implementation

--- LPBC/PIcontroller.py
+++ LPBC/PIcontroller.py
@@ -25,14 +25,22 @@ class PIcontroller():
             else:  # if controller is saturated but target/phasor error is different direction then turn off anti-windup
                 currentIntError_ang = phasor_error_ang[phase]
             self.intError_ang[phase] += currentIntError_ang
-            self.Pcmd_pu[phase] = (self.Kp_ang[phase] * phasor_error_ang[phase]) + self.Ki_ang[phase] * self.intError_ang[phase]
+
+            #self.Pcmd_pu[phase] = (self.Kp_ang[phase] * phasor_error_ang[phase]) + self.Ki_ang[phase] * self.intError_ang[phase]
+
+            #SWITCHED Q to control angle for 33NF (low X/R ratio)
+            self.Qcmd_pu[phase] = (self.Kp_ang[phase] * phasor_error_ang[phase]) + self.Ki_ang[phase] * self.intError_ang[phase]
 
             if sat_arrayQ[phase] == 0 and np.sign(self.intError_mag[phase]) == np.sign(phasor_error_mag[phase]):
                 currentIntError_mag = phasor_error_mag[phase] * sat_arrayQ[phase]
             else:
                 currentIntError_mag = phasor_error_mag[phase]
             self.intError_mag[phase] += currentIntError_mag
-            self.Qcmd_pu[phase] = (self.Kp_mag[phase] * phasor_error_mag[phase]) + self.Ki_mag[phase] * self.intError_mag[phase]
+
+            #self.Qcmd_pu[phase] = (self.Kp_mag[phase] * phasor_error_mag[phase]) + self.Ki_mag[phase] * self.intError_mag[phase]
+
+            #SWITCHED P to control magnitude for 33NF (low X/R ratio)
+            self.Pcmd_pu[phase] = (self.Kp_mag[phase] * phasor_error_mag[phase]) + self.Ki_mag[phase] * self.intError_mag[phase]
 
         print('self.intError_ang : ' + str(self.intError_ang))
         print('self.intError_mag : ' + str(self.intError_mag))
