6fb4954d20cf122df65a08e67241169101e78248
ENERGISE <jasperpakshong@gmail.com>
2020-05-18 18:03:39 -0700

    CIL command

--- flexlab/CIL_debug2.py
+++ flexlab/CIL_debug2.py
@@ -40,10 +40,10 @@ finally:
 #Q1, Q2, Q3 = 2, 4, 6
 
 #RESET REGISTERS BACK TO ZERO TO RESTART OR END CIL TESTING
-P1, P2, P3 = 0, 0, 0
+P1, P2, P3 = 50, 0, 0
 Q1, Q2, Q3 = 0, 0, 0
 
-sign_vec = [0,0,
+sign_vec = [1,0,
             0,0,
             0,0]
 sign_base = 2**5 * sign_vec[0] + 2**4 * sign_vec[1] + 2**3 * sign_vec[2] + 2**2 * sign_vec[3] + 2**1 * sign_vec[4] + 2**0  * sign_vec[5]
