e1bc59a795290277f6bd857cfc0953ff91e31a65
ENERGISE <jasperpakshong@gmail.com>
2020-05-21 16:50:50 -0700

    spbc 13bal T3.1

--- SPBC/spbc-example-jasper.py
+++ SPBC/spbc-example-jasper.py
@@ -18,8 +18,8 @@ logging.basicConfig(level="INFO", format='%(asctime)s - %(name)s - %(message)s')
 #print('phases on network:',phase_size)
 
 # SETTINGS
-lpbc_phases = ['a','b','c'] # [INPUT HERE]
-lpbc_nodeIDs = ['18','26'] # [INPUT HERE]
+lpbc_phases = ['a'] # [INPUT HERE]
+lpbc_nodeIDs = ['675'] # [INPUT HERE]
 angle_unit = 'radians' # - 'degrees' or 'radians' - settled on radians
 
 TV_load = False # [INPUT HERE] - set whether SPBC cycles through load values or holds constant
