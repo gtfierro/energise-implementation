fa6dad4deab0fffd61efa320a7f608911c5eef96
ENERGISE <jasperpakshong@gmail.com>
2020-05-21 10:59:48 -0700

    api dyn update

--- LPBC/api_dyn.py
+++ LPBC/api_dyn.py
@@ -9,15 +9,16 @@ inv = 1
     
 loop = 0
 #loop = 1
+loop = 2
     
 t0 = time.time()
 
 #~~~~~~~~~~~
 # INVERTER
 
-command = f"http://flexgrid-s1.dhcp.lbl.gov:9090/control?dyn_P_ctrl={Pcmd_perc_phase},inv_id={inv}"
+# command = f"http://flexgrid-s1.dhcp.lbl.gov:9090/control?dyn_P_ctrl={Pcmd_perc_phase},inv_id={inv}"
 # command = 'http://flexgrid-s1.dhcp.lbl.gov:9090/status'
-print(command)
+# print(command)
 
 if loop == 0:
 
@@ -34,3 +35,15 @@ if loop == 0:
 #     print(f'time to execute: {time.time()-t0}')
 #     print(r)
 #     print('api cmd:', command, dt.datetime.now())
+
+if loop == 2:
+    Pcmd_perc_phase = 5.
+    for itr in range(0,10):
+        t0 = time.time()
+        command = f"http://flexgrid-s1.dhcp.lbl.gov:9090/control?dyn_P_ctrl={Pcmd_perc_phase},inv_id={inv}"
+        r = requests.get(command)
+        print(f'time to execute: {time.time()-t0}')
+        print(r.status_code)
+        print('api cmd:', command, dt.datetime.now())
+        Pcmd_perc_phase += 5
+        print(Pcmd_perc_phase)
