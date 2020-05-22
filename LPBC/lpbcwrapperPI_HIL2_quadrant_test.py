c4582b6c41c6c4bb4825ee45642dfc305975fda3
ENERGISE <jasperpakshong@gmail.com>
2020-05-22 12:30:24 -0700

    quadrant test fixes

--- LPBC/lpbcwrapperPI_HIL2_quadrant_test.py
+++ LPBC/lpbcwrapperPI_HIL2_quadrant_test.py
@@ -693,7 +693,7 @@ class lpbcwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
 
     def modbustoOpal_quadrant(self, Pcmd_kVA, Qcmd_kVA, Pact, Qact, act_idxs, client):
         'quadrant debug'
-        if self.iteration_counter <= 2: #Q1
+        if self.iteration_counter <= 2: #Q1 
             Pcmd_kVA, Qcmd_kVA = [0.1], [0.1]
             Pact, Qact = (-np.array(Pcmd_kVA)).tolist(), (-np.array(Qcmd_kVA)).tolist() # so that it will go through the register setting process
         if self.iteration_counter > 2 and self.iteration_counter <= 4: #Q2
@@ -732,15 +732,15 @@ class lpbcwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
                     inv_act_idxs_registers[i] = inv_2
                 elif inv_act_idxs_registers[i] == 3:
                     inv_act_idxs_registers[i] = inv_3
-            # Quadrant value mapping - 1: [-1, -1], 2: [1, -1], 3: [-1, 1], 4: [1, 1]
-            # multipliers to inverter values [P, Q] - so 4 corresponds to injecting P and Q.
+            # value mapping - 1: [-1, -1], 2: [1, -1], 3: [-1, 1], 4: [1, 1]
+            # multipliers to inverter values [P, Q] - positive inverter values corresponds to injecting P and Q (value 4)
             for i, j in zip(pq_changed, range(len(act_idxs_registers))):  # determines exact quadrant for inverter
                 if Pcmd_kVA[i] >= 0 and Qcmd_kVA[i] >= 0:  # quadrant 1
-                    value[j] = 1
+                    value[j] = 4
                 if Pcmd_kVA[i] < 0 and Qcmd_kVA[i] >= 0:  # quadrant 2
                     value[j] = 3
                 if Pcmd_kVA[i] < 0 and Qcmd_kVA[i] < 0:  # quadrant 3
-                    value[j] = 4
+                    value[j] = 1
                 if Pcmd_kVA[i] >= 0 and Qcmd_kVA[i] < 0:  # quadrant 4
                     value[j] = 2
             try:
@@ -760,8 +760,7 @@ class lpbcwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
             return
 
     def modbustoOpal(self, nphases, Pcmd_kVA, Qcmd_kVA, ORT_max_VA, localSratio, client ):
-        Pcmd_VA = -1 * (Pcmd_kVA * 1000) #sign negation is convention of modbus
-        Qcmd_VA = -1 * (Qcmd_kVA * 1000) #sign negation is convention of modbus
+
         id = 3
         # Connect to client
         client.connect()
