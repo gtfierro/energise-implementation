6bc1a7a732f665a50f3d649b4100d4c5322262a3
leo32393 <leo32393@berkeley.edu>
2020-05-27 14:28:43 -0700

    test

--- flexlab/main_flexlab.py
+++ flexlab/main_flexlab.py
@@ -68,12 +68,12 @@ functions from ORT_modbus_energise:
 # feeder =
 # Choose from [13NF_bal, 13NF_unbal]
 pathname = 'sw_mat_HIL2/'
-filename = 'HIL_switch_matrix_PL0001_CIL.xlsx'
+filename = 'HIL_switch_matrix_13NF_unbal_CIL.xlsx'
 filepath = pathname+filename
 
 
-test_ID = 'T9.3' # which test case you want to run
-sim_length_min = 50 # amount of time to record for in minutes
+test_ID = 'T8.1' # which test case you want to run
+sim_length_min = 1 # amount of time to record for in minutes
 # In[run]:
 
 # run functions...
