0f0f484983db89168b4def4486b73bcc1887cb44
ENERGISE <jasperpakshong@gmail.com>
2020-05-28 12:56:12 -0700

    spbc_new update

--- SPBC/spbc_new/main_run_3_new.py
+++ SPBC/spbc_new/main_run_3_new.py
@@ -29,20 +29,18 @@ print(datetime.datetime.fromtimestamp(ts))
 # =============================================================================
 
 'IEEE13_BALANCED'
-filepath = "IEEE13_bal/"
-modelpath = filepath + "016_GB_IEEE13_balance_reform.xlsx"
-
-loadfolder = "IEEE13_bal/"
-loadpath = loadfolder + "016_GB_IEEE13_balance_norm03.xlsx"
+# filepath = "IEEE13_bal/"
+# modelpath = filepath + "016_GB_IEEE13_balance_reform.xlsx"
+# loadfolder = "IEEE13_bal/"
+# loadpath = loadfolder + "016_GB_IEEE13_balance_norm03.xlsx"
 
 
 'UCB33'
 # =============================================================================
-# filepath = "UCB33/"
-# modelpath = filepath + "005_GB_UCB33_opal_v2_SPBCform_modified_length.xlsx"
-# 
-# loadfolder = "UCB33/"
-# loadpath = loadfolder + "005_GB_UCB33_time_sigBuilder_Q.xlsx"
+filepath = "UCB33/"
+modelpath = filepath + "005_GB_UCB33_opal_v3.xlsx"
+loadfolder = "UCB33/"
+loadpath = loadfolder + "005_GB_UCB33_time_sigBuilder_Q_13_14_norm03.xlsx"
 # =============================================================================
 
 
@@ -70,30 +68,15 @@ loadpath = loadfolder + "016_GB_IEEE13_balance_norm03.xlsx"
 
 # Specify substation kV, kVA bases, and the number of timesteps in the load data
 'IEEE13'
-subkVbase_phg = 4.16/np.sqrt(3)
-subkVAbase = 5000.
-timesteps = 3 #int((16-8)*60)
+# subkVbase_phg = 4.16/np.sqrt(3)
+# subkVAbase = 5000.
+# timesteps = 3 #int((16-8)*60)
 
 'UCB33'
 # =============================================================================
-# subkVbase_phg = 12.47/np.sqrt(3)
-# #subkVbase_phg = 4.16/np.sqrt(3)
-# subkVAbase = 3000.
-# timesteps = 3 #int((16-8)*60)
-# =============================================================================
-
-'IEEE37'
-# =============================================================================
-# subkVbase_phg = 4.8/np.sqrt(3)
-# subkVAbase = 2500.  #1500000.
-# timesteps = 20
-# =============================================================================
-
-'BU0001'
-# =============================================================================
-# subkVbase_phg = 4.37/np.sqrt(3)
-# subkVAbase = 90000.  #1500000.
-# timesteps = 3
+subkVbase_phg = 12.47/np.sqrt(3)
+subkVAbase = 3000.
+timesteps = 2 #int((16-8)*60)
 # =============================================================================
 
 'PL0001'
@@ -102,14 +85,6 @@ timesteps = 3 #int((16-8)*60)
 # subkVAbase = 1500. #1000000.  #1500000.
 # timesteps = 2
 # =============================================================================
-
-'IEEE118 & IEE14'
-# =============================================================================
-# subkVbase_phg = 138/np.sqrt(3)
-# subkVAbase = 100000.
-# timesteps = 2
-# =============================================================================
-
 
 plot = 0 #turn plot on/off
 
@@ -120,10 +95,10 @@ month, day, hour, minute = date.month, date.day, date.hour, date.minute
 timestepcur = 0 #int(11*60) # [INPUT HERE] Manual input of start time
 
 # Input atcuator nodes and capacities ('nodeID': phases, kVA capacity)
-act_init = {
-        '652': {'a': 200, 'b': 0, 'c':0},
-        '671': {'a': 100, 'b': 0, 'c':0},
-        '692': {'a': 200, 'b': 0, 'c':0}
+act_init = { '18': {'a': 500, 'b': 500, 'c':500}
+        # '652': {'a': 200, 'b': 0, 'c':0},
+        # '671': {'a': 100, 'b': 0, 'c':0},
+        # '692': {'a': 200, 'b': 0, 'c':0}
             }
 
 # Input constnats for PV forecasting
