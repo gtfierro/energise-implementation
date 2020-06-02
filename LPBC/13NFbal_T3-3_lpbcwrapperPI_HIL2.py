fb7aba2c7e79a223f08127024ae5957496a7576b
ENERGISE <jasperpakshong@gmail.com>
2020-06-01 17:06:45 -0700

    +100 Q offset

--- LPBC/13NFbal_T3-3_lpbcwrapperPI_HIL2.py
+++ LPBC/13NFbal_T3-3_lpbcwrapperPI_HIL2.py
@@ -599,7 +599,7 @@ class lpbcwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
             print(f'absolute value of P/Q:{Pcmd_VA},{Qcmd_VA}')
 
             Pcmd_perc = Pcmd_VA / inv_Pmax * 100  # Pcmd to inverters must be a percentage of Pmax
-            Qcmd_perc = Qcmd_VA / inv_Qmax * 100 # Qcmd to inverters must be a percentage of Qmax
+            Qcmd_perc = Qcmd_VA + 100 / inv_Qmax * 100 # Qcmd to inverters must be a percentage of Qmax
 
             act_idxs = act_idxs.tolist()
             for i in range(len(Pcmd_perc)):  # checks Pcmd for inverter limit
