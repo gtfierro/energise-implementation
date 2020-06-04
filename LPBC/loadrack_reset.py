767b95371eda32d4427384b19d6b922d7c1cb8a3
ENERGISE <jasperpakshong@gmail.com>
2020-06-04 15:56:54 -0700

    loadrack reset debug

--- LPBC/loadrack_reset.py
+++ LPBC/loadrack_reset.py
@@ -8,7 +8,7 @@ groups = [0,1,2]
 P_init = 1000
 
 for group in groups:
-    command = f"http://131.243.41.59:9090/control?group_id={group},P_ctrl={P_init}")
+    command = f"http://131.243.41.59:9090/control?group_id={group},P_ctrl={P_init}"
     r = requests.get(command)
     print(r.status_code)
     print('api cmd:', command, dt.datetime.now())
