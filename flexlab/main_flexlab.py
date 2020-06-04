06dec7103658d976a7bfb4e76cdd560ffa494114
ENERGISE <jasperpakshong@gmail.com>
2020-06-04 14:47:57 -0700

    sw -> 13unbal T8.1

--- flexlab/main_flexlab.py
+++ flexlab/main_flexlab.py
@@ -64,12 +64,12 @@ functions from ORT_modbus_energise:
 pathname = 'sw_mat_HIL2/'
 # filename = 'HIL2_switch_matrix_13NF_bal.xlsx'
 # filename = 'HIL2_switch_matrix_13NF_bal_CILdebug.xlsx'
-filename = 'HIL2_switch_matrix_13NF_bal_lowact.xlsx'
+filename = 'HIL_switch_matrix_13NF_unbal_offset.xlsx'
 filepath = pathname+filename
 
 
-test_ID = 'T12.3' # which test case you want to run
-sim_length_min = 50 # amount of time to record for in minutes
+test_ID = 'T8.3' # which test case you want to run
+sim_length_min = 5/60 # amount of time to record for in minutes
 # sim_length_min = 60/60
 # In[run]:
 
