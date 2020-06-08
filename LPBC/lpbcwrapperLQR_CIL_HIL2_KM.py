6098c87ef750c8145b8837686445c215742fd23c
keith <keithm@berkeley.edu>
2020-06-07 23:06:25 -0700

    small edit

--- LPBC/lpbcwrapperLQR_CIL_HIL2_KM.py
+++ LPBC/lpbcwrapperLQR_CIL_HIL2_KM.py
@@ -290,8 +290,8 @@ class lpbcwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
         self.testcase = cfg['testcase']
         self.saveVmagandangPlot = 1
         self.saveZesterrorPlot = 0
-        # self.HistLength = 100
-        self.HistLength = 10
+        self.HistLength = 50
+        # self.HistLength = 10
         self.VmagHist = np.zeros((self.nphases,self.HistLength))
         self.VangHist = np.zeros((self.nphases,self.HistLength))
         self.ZeffkErrorHist = np.zeros(self.HistLength)
