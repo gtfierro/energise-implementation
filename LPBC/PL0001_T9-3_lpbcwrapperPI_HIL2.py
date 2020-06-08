7e5b85350f2c28ee869e24979880f0089ab83cd9
ENERGISE <jasperpakshong@gmail.com>
2020-06-08 11:58:18 -0700

    rate = 15

--- LPBC/PL0001_T9-3_lpbcwrapperPI_HIL2.py
+++ LPBC/PL0001_T9-3_lpbcwrapperPI_HIL2.py
@@ -1246,7 +1246,7 @@ inverterScaling = 1000/1 #@JASPER TODO (and possibly ORT_max_kVA) --> 1000/1 for
 loadScaling = 350
 CILscaling = 500/3.3
 
-rate = 30
+rate = 15
 
 lpbcdict = dict()
 for lpbcCounter, key in enumerate(lpbcidx):
