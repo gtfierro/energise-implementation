b8a9c7ba283811ea81e518a89fa472543be1375b
ENERGISE <jasperpakshong@gmail.com>
2020-06-08 11:57:46 -0700

    back to offset with even lower gains

--- LPBC/PL0001_T9-3_lpbcwrapperPI_HIL2.py
+++ LPBC/PL0001_T9-3_lpbcwrapperPI_HIL2.py
@@ -119,7 +119,7 @@ class lpbcwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
             #9.3
             alph = 1
             beta = 1
-            lam_i = 1.2
+            lam_i = 1
             kp_ang = [0.048*alph, 0.048*alph, 0.048*alph]
             ki_ang = [0.028*alph, 0.028*alph, 0.028*alph]
             kp_mag = [2*beta, 2*beta, 2*beta]
