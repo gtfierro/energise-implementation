d164b4d874edd1b8e2a7c33cff8c3f991f48242a
ENERGISE <jasperpakshong@gmail.com>
2020-06-09 09:40:52 -0700

    offset mode 2 debug

--- LPBC/13NFbal_T3-3_lpbcwrapperPI_HIL2_offset.py
+++ LPBC/13NFbal_T3-3_lpbcwrapperPI_HIL2_offset.py
@@ -710,14 +710,14 @@ class lpbcwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
                     inv = inv.item()  # changes data type
                 urls.append(f"http://131.243.41.48:9090/control?dyn_P_ctrl={Pcmd_perc_phase},dyn_Q_ctrl={Qcmd_perc_phase},inv_id={inv}")
 
-        # responses = map(session.get, urls)
-        # results = [resp.result() for resp in responses]
-        # for i in range(nphases):
-        #     if results[i].status_code == 200:
-        #         commandReceipt[i] = 'success'
-        #     else:
-        #         commandReceipt[i] = 'failure'
-        # print(f'INV COMMAND RECEIPT: {commandReceipt}')
+        responses = map(session.get, urls)
+        results = [resp.result() for resp in responses]
+        for i in range(nphases):
+            if results[i].status_code == 200:
+                commandReceipt[i] = 'success'
+            else:
+                commandReceipt[i] = 'failure'
+        print(f'INV COMMAND RECEIPT: {commandReceipt}')
         if self.offset_mode == 1 or self.offset_mode == 2:
             try:
                 self.client.connect()
@@ -728,7 +728,7 @@ class lpbcwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
                 print(e)        
             finally:
                 self.client.close()
-        return #commandReceipt
+        return commandReceipt
 
     def API_inverters(self, act_idxs, Pcmd_kVA, Qcmd_kVA, inv_Pmax, inv_Qmax, flexgrid):
         Pcmd_VA = abs(Pcmd_kVA*1000) #abs values for working only in quadrant 1. Will use modbus to determine quadrant
