0d742bd83e9437b68d292ace0344f32ad952a883
keith <keithm@berkeley.edu>
2020-06-09 08:44:37 -0700

    minor edit

--- LPBC/lpbcwrapperLQR_CIL_HIL2_KM.py
+++ LPBC/lpbcwrapperLQR_CIL_HIL2_KM.py
@@ -1101,8 +1101,8 @@ class lpbcwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
 
             # self.Pcmd_pu = np.zeros(3)
             # self.Qcmd_pu = np.zeros(3) #HHHERE debug
-            self.Pcmd_pu = np.ones(3)*.1
-            self.Qcmd_pu = np.ones(3)*.1
+            self.Pcmd_pu = np.ones(3)*.2
+            self.Qcmd_pu = np.ones(3)*.2
 
             # self.localkVAbase = self.network_kVAbase/self.localSratio, so this assumes that the power injections will later get multiplied by self.localSratio
             self.Pcmd_kVA = self.Pcmd_pu * self.localkVAbase #these are positive for power injections, not extractions
