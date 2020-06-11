800e56731d875d024fd1cd737b0eafa9708813d3
keith <keithm@berkeley.edu>
2020-06-11 16:55:50 -0700

    spbc keith edit

--- SPBC/spbc-example-keith.py
+++ SPBC/spbc-example-keith.py
@@ -139,7 +139,7 @@ if constant_phasor == True:
         if testID == 'T9.3':
             lpbc_phases = ['a','b','c']
             lpbc_nodeIDs = ['N_300063911']
-            cons_Vmag = [0.99,0.99,0.99]
+            cons_Vmag = [0.98,0.98,0.98]
             cons_Vang = [0 - 3, -120 - 3, 120 - 3]
 
     print('WARNING: constant_phasor ON')
