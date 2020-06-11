4ae9bcf4dc54ee61852b9b6f39ad18e790579543
keith <keithm@berkeley.edu>
2020-06-11 12:58:10 -0700

    changed sim_length

--- flexlab/main_flexlab.py
+++ flexlab/main_flexlab.py
@@ -64,11 +64,13 @@ functions from ORT_modbus_energise:
 pathname = 'sw_mat_HIL2/'
 # filename = 'HIL2_switch_matrix_13NF_bal.xlsx'
 # filename = 'HIL2_switch_matrix_13NF_bal_CILdebug.xlsx'
-filename = 'HIL2_switch_matrix_13NF_bal_offset.xlsx'
+# filename = 'HIL2_switch_matrix_13NF_bal_offset.xlsx'
+filename = 'HIL_switch_matrix_13NF_unbal_offset.xlsx'
 filepath = pathname+filename
 
 # test_ID = 'T12.3' # which test case you want to run
-test_ID = 'T3.3' # which test case you want to run
+# test_ID = 'T3.3' # which test case you want to run
+test_ID = 'T8.1'
 sim_length_min = 50 # amount of time to record for in minutes
 # sim_length_min = 1/60
 # In[run]:
