fb448b52634f838c76c0df4d802f3370f8a60d69
keith <keithm@berkeley.edu>
2020-06-10 17:27:55 -0700

    changed sim_length

--- flexlab/main_flexlab.py
+++ flexlab/main_flexlab.py
@@ -69,8 +69,8 @@ filepath = pathname+filename
 
 # test_ID = 'T12.3' # which test case you want to run
 test_ID = 'T3.3' # which test case you want to run
-# sim_length_min = 50 # amount of time to record for in minutes
-sim_length_min = 1/60
+sim_length_min = 50 # amount of time to record for in minutes
+# sim_length_min = 1/60
 # In[run]:
 
 # run functions...
