a371c85be3247dd72d367fda6e6bef86346199e1
ENERGISE <jasperpakshong@gmail.com>
2020-06-12 15:45:37 -0700

    spbc iter 33NF 3.3

--- LPBC/33NFbal_T3_lpbcwrapperPI_HIL2.py
+++ LPBC/33NFbal_T3_lpbcwrapperPI_HIL2.py
@@ -117,7 +117,7 @@ class lpbcwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
             # ki_mag = [3.5004*beta,3.5004*beta,3.5004*beta]
 
             # 8.1 (33NF)
-            alph = 0.5
+            alph = 0.1
             beta = 0.5
             kp_ang = [0.001 * alph, 0.001 * alph, 0.001 * alph]
             ki_ang = [0.2 * alph, 0.2 * alph, 0.2 * alph]
