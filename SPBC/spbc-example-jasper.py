d0887795f951e0c63822dcfbe739e61d2f867f9d
ENERGISE <jasperpakshong@gmail.com>
2020-09-25 15:54:29 -0700

    predictive grid API and plot update with overlay

--- SPBC/spbc-example-jasper.py
+++ SPBC/spbc-example-jasper.py
@@ -27,8 +27,8 @@ start_hour = 11             # [INPUT HERE]
 
 dummy_ref = True            # [INPUT HERE]
 constant_phasor = True      # [INPUT HERE]
-feederID =  '13bal'        # [INPUT HERE] 13bal, 13unbal, UCB33, PL0001
-testID = 'T12.3' 
+feederID =  '13unbal'        # [INPUT HERE] 13bal, 13unbal, UCB33, PL0001
+testID = 'T8.2' 
 
 if dummy_ref == True:
     print('WARNING: constant_ref ON')
