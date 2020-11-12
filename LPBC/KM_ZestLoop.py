e942f890f5b61b397d373a72c532f3cb87bf6815
keith <keithm@berkeley.edu>
2020-11-12 15:12:32 -0800

    edit

--- LPBC/KM_ZestLoop.py
+++ LPBC/KM_ZestLoop.py
@@ -2,8 +2,15 @@
 
 from KM_Zestwrapper_CILfunc import *
 
+caution = 1.1
+stepLength = 10 #this is rate in Zestwrapper in seconds
 HistLength = 3
 # Zeffk_init_mults = [.5, .75, 1.25, 1.5, 2]
 Zeffk_init_mults = [.75, 1.25]
-for i in len(Zeffk_init_mults):
+for i in np.arange(len(Zeffk_init_mults)):
     buildZestimators(Zeffk_init_mults[i],  HistLength)
+
+    loop.run_forever()
+    await asyncio.sleep(caution * stepLength * HistLength)
+    loop.stop()
+    loop.close()
