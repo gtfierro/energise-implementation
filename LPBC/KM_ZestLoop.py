104969e1013d9cbb2e7ac242937a44a478c78703
keith <keithm@berkeley.edu>
2020-11-12 15:27:45 -0800

    edit

--- LPBC/KM_ZestLoop.py
+++ LPBC/KM_ZestLoop.py
@@ -9,15 +9,16 @@ HistLength = 3
 # Zeffk_init_mults = [.5, .75, 1.25, 1.5, 2]
 Zeffk_init_mults = [.75, 1.25]
 
-print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ DONE SLEEPING')
-asyncio.sleep(caution)
-print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ DONE SLEEPING')
+# print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ DONE SLEEPING')
+# asyncio.sleep(caution)
+# print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ DONE SLEEPING')
 
 for i in np.arange(len(Zeffk_init_mults)):
     buildZestimators(Zeffk_init_mults[i],  HistLength)
 
     loop = asyncio.get_event_loop()
     loop.run_forever()
+    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
     # await asyncio.sleep(caution * stepLength * HistLength)
     asyncio.sleep(caution * stepLength * HistLength)
     print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ DONE SLEEPING')
