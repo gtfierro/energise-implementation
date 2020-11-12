b9ef691b331f2836b062f0968fccf6368f2e99d6
keith <keithm@berkeley.edu>
2020-11-12 15:25:42 -0800

    edit

--- LPBC/KM_ZestLoop.py
+++ LPBC/KM_ZestLoop.py
@@ -8,6 +8,11 @@ stepLength = 10 #this is rate in Zestwrapper in seconds
 HistLength = 3
 # Zeffk_init_mults = [.5, .75, 1.25, 1.5, 2]
 Zeffk_init_mults = [.75, 1.25]
+
+print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ DONE SLEEPING')
+asyncio.sleep(caution)
+print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ DONE SLEEPING')
+
 for i in np.arange(len(Zeffk_init_mults)):
     buildZestimators(Zeffk_init_mults[i],  HistLength)
 
