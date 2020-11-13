7920a055de2f6dfe031a9465c0707a00341e0194
keith <keithm@berkeley.edu>
2020-11-13 14:08:17 -0800

    edit

--- LPBC/plotZestError.py
+++ LPBC/plotZestError.py
@@ -10,11 +10,16 @@ current_directory = os.getcwd()
 simFolderPATH = os.path.join(current_directory, 'simulationPlots')
 # resultsPATH = os.path.join(simFolderPATH, f'feeder:13bal_bus:675')
 # fileNames = ['ZestData_eps=0.5',  'ZestData_eps=0.75',  'ZestData_eps=1.25', 'ZestData_eps=1.5', 'ZestData_eps=2']
-resultsPATH = os.path.join(simFolderPATH, f'feeder:13bal_bus:675_5runs_pertrub:p01_lam:p95_G:100')
-fileNames = ['ZestData_λ=0.5',  'ZestData_λ=0.75',  'ZestData_λ=1.25', 'ZestData_λ=1.5', 'ZestData_λ=2']
+
+# resultsPATH = os.path.join(simFolderPATH, f'feeder:13bal_bus:675_Wcaps_5runs_perturb:p01_lam:p95_G:100')
+# fileNames = ['ZestData_λ=0.5',  'ZestData_λ=0.75',  'ZestData_λ=1.25', 'ZestData_λ=1.5', 'ZestData_λ=2']
+# labelNames = [r'$\epsilon$ = 0.5', r'$\epsilon$ = 0.75', r'$\epsilon$ = 1.25', r'$\epsilon$ = 1.5', r'$\epsilon$ = 2']
+
+resultsPATH = os.path.join(simFolderPATH, f'feeder:13bal_bus:675')
+fileNames = ['ZestData_eps=0.5',  'ZestData_eps=0.75',  'ZestData_eps=1.25', 'ZestData_eps=1.5', 'ZestData_eps=2.0']
 labelNames = [r'$\epsilon$ = 0.5', r'$\epsilon$ = 0.75', r'$\epsilon$ = 1.25', r'$\epsilon$ = 1.5', r'$\epsilon$ = 2']
 
-plotZest = 0
+plotZest = 1
 plotVmag = 1
 plotVmag_noPerturbToo = 1
 VmagIndex = 1
