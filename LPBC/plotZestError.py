0a8361770319c52e971cff7c7e62c8d68d960531
keith <keithm@berkeley.edu>
2020-11-13 08:31:17 -0800

    edit

--- LPBC/plotZestError.py
+++ LPBC/plotZestError.py
@@ -14,44 +14,70 @@ resultsPATH = os.path.join(simFolderPATH, f'feeder:13bal_bus:675_5runs_pertrub:p
 fileNames = ['ZestData_λ=0.5',  'ZestData_λ=0.75',  'ZestData_λ=1.25', 'ZestData_λ=1.5', 'ZestData_λ=2']
 labelNames = [r'$\epsilon$ = 0.5', r'$\epsilon$ = 0.75', r'$\epsilon$ = 1.25', r'$\epsilon$ = 1.5', r'$\epsilon$ = 2']
 
-for i in np.arange(len(fileNames)):
-    print('fileNames[i] ', fileNames[i])
-    Z_df = pd.read_csv(os.path.join(resultsPATH, f'{fileNames[i]}.csv'), index_col=0)
-    Z_array = Z_df[['Zth Estimation Error']].to_numpy()
-    plt.plot(Z_array, label=labelNames[i]) # this uses the filename for the legend
-# plt.title(r'$Z_{Th}$ Estimation Error')
-# plt.ylabel('Frobenius Norm Error')
-plt.ylabel(r'$Z_{Th}$ Frobenius Norm Error')
-plt.xlabel('Timestep')
-plt.legend()
-plt.show()
+plotZest = 0
+plotVmag = 1
+plotVmag_noPerturbToo = 1
+VmagIndex = 1
 
-for i in np.arange(len(fileNames)):
-    Z_df = pd.read_csv(os.path.join(resultsPATH, f'{fileNames[i]}.csv'), index_col=0)
-    Z_array = Z_df[['Gt']].to_numpy()
-    plt.plot(Z_array, label=labelNames[i]) # this uses the filename for the legend
-# plt.title('$F$ Magnitude')
-plt.ylabel('Frobenius Norm of $F$')
-plt.xlabel('Timestep')
-plt.legend()
-plt.show()
+if plotZest:
+    for i in np.arange(len(fileNames)):
+        print('fileNames[i] ', fileNames[i])
+        Z_df = pd.read_csv(os.path.join(resultsPATH, f'{fileNames[i]}.csv'), index_col=0)
+        Z_array = Z_df[['Zth Estimation Error']].to_numpy()
+        plt.plot(Z_array, label=labelNames[i]) # this uses the filename for the legend
+    # plt.title(r'$Z_{Th}$ Estimation Error')
+    # plt.ylabel('Frobenius Norm Error')
+    plt.ylabel(r'$Z_{Th}$ Frobenius Norm Error')
+    plt.xlabel('Timestep')
+    plt.legend()
+    plt.show()
 
-nodeNames = ['Va Magnitude', 'Vb Magnitude', 'Vc Magnitude']
-Z_df = pd.read_csv(os.path.join(resultsPATH, f'{fileNames[0]}.csv'), index_col=0)
-Z_array = Z_df[['Va Magnitude', 'Vb Magnitude', 'Vc Magnitude']].to_numpy()
-plt.plot(Z_array[:,0], label='phase A')
-plt.plot(Z_array[:,1], label='phase B')
-plt.plot(Z_array[:,2], label='phase C')
-    # if i == 0:
-    #     plt.plot(Z_array, label='phase A') # this uses the filename for the legend
-    # elif i == 1:
-    #     plt.plot(Z_array, label='phase B') # this uses the filename for the legend
-    # else:
-    #     plt.plot(Z_array, label='phase C') # this uses the filename for the legend
-# plt.title('Node 675 Voltage Magnitude')
-plt.ylabel('Voltage Magnitude [p.u.]')
-plt.xlabel('Timestep')
-plt.legend()
-plt.show()
+    for i in np.arange(len(fileNames)):
+        Z_df = pd.read_csv(os.path.join(resultsPATH, f'{fileNames[i]}.csv'), index_col=0)
+        Z_array = Z_df[['Gt']].to_numpy()
+        plt.plot(Z_array, label=labelNames[i]) # this uses the filename for the legend
+    # plt.title('$F$ Magnitude')
+    plt.ylabel('Frobenius Norm of $F$')
+    plt.xlabel('Timestep')
+    plt.legend()
+    plt.show()
+
+if plotVmag:
+    nodeNames = ['Va Magnitude', 'Vb Magnitude', 'Vc Magnitude']
+    Z_df = pd.read_csv(os.path.join(resultsPATH, f'{fileNames[VmagIndex]}.csv'), index_col=0)
+    Z_array = Z_df[['Va Magnitude', 'Vb Magnitude', 'Vc Magnitude']].to_numpy()
+    plt.plot(Z_array[:,0], label='phase A')
+    plt.plot(Z_array[:,1], label='phase B')
+    plt.plot(Z_array[:,2], label='phase C')
+        # if i == 0:
+        #     plt.plot(Z_array, label='phase A') # this uses the filename for the legend
+        # elif i == 1:
+        #     plt.plot(Z_array, label='phase B') # this uses the filename for the legend
+        # else:
+        #     plt.plot(Z_array, label='phase C') # this uses the filename for the legend
+    # plt.title('Node 675 Voltage Magnitude')
+    plt.ylabel('Voltage Magnitude [p.u.]')
+    plt.xlabel('Timestep')
+    plt.legend()
+
+    if plotVmag_noPerturbToo:
+        nodeNames = ['Va Magnitude', 'Vb Magnitude', 'Vc Magnitude']
+        Z_df = pd.read_csv(os.path.join(resultsPATH, f'ZestData_noPerturb.csv'), index_col=0)
+        Z_array = Z_df[['Va Magnitude', 'Vb Magnitude', 'Vc Magnitude']].to_numpy()
+        plt.plot(Z_array[:,0], label='phase A')
+        plt.plot(Z_array[:,1], label='phase B')
+        plt.plot(Z_array[:,2], label='phase C')
+            # if i == 0:
+            #     plt.plot(Z_array, label='phase A') # this uses the filename for the legend
+            # elif i == 1:
+            #     plt.plot(Z_array, label='phase B') # this uses the filename for the legend
+            # else:
+            #     plt.plot(Z_array, label='phase C') # this uses the filename for the legend
+        # plt.title('Node 675 Voltage Magnitude')
+        plt.ylabel('Voltage Magnitude [p.u.]')
+        plt.xlabel('Timestep')
+        plt.legend()
+
+    plt.show()
 
 # ['Zth Estimation Error', 'Gt', 'Va Magnitude', 'Vb Magnitude', 'Vc Magnitude']
