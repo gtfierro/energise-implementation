f4ed3abd116a6e42077758eb7c0d4dc0116217a7
keith <keithm@berkeley.edu>
2020-11-13 17:39:09 -0800

    edit

--- LPBC/KM_Zestwrapper_CIL.py
+++ LPBC/KM_Zestwrapper_CIL.py
@@ -311,7 +311,7 @@ class Zestwrapper(pbc.LPBCProcess): #this is related to super(), inherits attrib
         self.saveZesterrorPlot = 1
         self.HistLength = 101
         # self.HistLength = 10
-        self.HistLength = 3
+        self.HistLength = 5
         self.VmagHist = np.zeros((self.nphases,self.HistLength))
         self.VangHist = np.zeros((self.nphases,self.HistLength))
         self.ZeffkErrorHist = np.zeros(self.HistLength)
